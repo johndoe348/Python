e571ab132e306eca66194d50d7714263ddba0be8
kmcginley <kmcginley@invaio.com>
2019-10-08 16:34:41 -0400

    up to challenge 6

--- challenge.py
+++ challenge.py
@@ -17,8 +17,29 @@ import re
 
 ### challenge 5
 
-url = 'http://pythonchallenge.com/pc/def/peak.html'
-text = requests.get(url).text
+# import pickle
+
+# #url = 'http://pythonchallenge.com/pc/def/peak.html'
+# url = 'http://pythonchallenge.com/pc/def/banner.p'
+
+# text = pickle.loads(requests.get(url).content)
+# print(text)
+# for x in text:
+#     print(''.join([k*v for k, v in x]))
+#     # this doesn't work because every iteration of a [(' ', 3), ('#', 3)], it prints each tuple in sequence, 
+#     # as opposed to adding all the tuples within the list together before printing
+#     # for k, v in x:
+#     #     print(''.join([k * v])
+
+
+### challenge 6
+
+url = 'http://pythonchallenge.com/pc/def/channel.html'
+
+text = requests.get(url).content
+print(text)
+
+
 
 
 
